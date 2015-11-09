102e5d4c23426005763ec663ecacf28eac39d7b0
kimby <kimby@student.ethz.ch>
2015-11-09 18:41:55 +0100

    implement imitation and migration

---
+++ code/cooperation.m
@@ -0,0 +1,211 @@
+function cooperation
+
+% parameters
+T=1.3;
+R=1;
+P=0.1;
+S=0;
+payoff=[R S;T P];
+
+L=49; % grid size
+L2=L^2;
+emptySiteProp=0.5;
+cooperatorProp=0.5;
+defectorProp=1.0-cooperatorProp;
+cooperator=1;
+defector=2;
+m=4; % # of von Neumann neighbors
+M=5; % Moore neighborhood
+imitationOn=true;
+migrationOn=false;
+rProb=0.05; % strategy reset probability
+qProb=0.05; % cooperation prefer probability
+
+grid=uint8(zeros(L));
+overallPayoff=zeros(L); % accumulated??
+iter=200;
+
+% initialize
+rng(1); % for reproduction
+occupiedSize=round(L2*(1-emptySiteProp));
+sitePos=randperm(L2,occupiedSize);
+cooperatorSize=round(occupiedSize*cooperatorProp);
+defectorSize=occupiedSize-cooperatorSize;
+cooperatorPos=sitePos(1:cooperatorSize);
+defectorPos=sitePos(cooperatorSize+1:end);
+
+grid(cooperatorPos)=cooperator;
+grid(defectorPos)=defector;
+
+% moore neighbor index
+MN=[0];
+for i=1:M
+    MN=[MN -i i];
+end
+
+levelCooperator=sum(sum(grid==cooperator))/size(sitePos,2)
+levelDefector=1-levelCooperator
+
+
+str='Grid, t=0';
+% colormap, w:empth, b:cooperator, r:defector
+cmap = [1, 1, 1,
+    0, 0, 1,
+    1, 0, 0];
+figure('Name',str)
+imagesc(grid);
+colormap(cmap);
+axis square
+
+    
+% start simulation
+for t=1:iter
+    disp(num2str(t,'iter=%d'));
+    
+    % update order
+    [R, C]=ind2sub([L L],randperm(L2));
+    
+    for i=1:L2
+        r=R(i);
+        c=C(i);
+        focal=grid(r,c);
+        
+        if focal==0 % empty
+            continue;
+        end
+        
+        op=getOverallPayoff(focal,r,c,L,grid,payoff);
+        
+        % migration
+        if migrationOn
+            highestPayoff=op;
+            highestrc=[r c];
+            
+            % test interaction
+            for MNr=MN
+                rr=r+MNr;
+                if rr < 1 || rr > L
+                   continue;
+                end
+                for MNc=MN
+                    cc=c+MNc;
+                    
+                    if cc < 1 || cc > L || ...
+                        (rr == r && cc == c) || ... % itself
+                        grid(rr,cc) ~= 0 % empty
+                       continue;
+                    end
+                    
+                    % fictitious payoff
+                    op=getOverallPayoff(focal,rr,cc,L,grid,payoff);
+        
+                    if op>highestPayoff
+                        highestPayoff=op;
+                        highestrc=[rr cc];
+                    end
+                end
+            end
+            
+            % migrate to the candidate site
+            if highestrc(1) ~= r || highestrc(2) ~= c
+                grid(highestrc(1),highestrc(2))=grid(r,c);
+                grid(r,c)=0;
+                r=highestrc(1);
+                c=highestrc(2);
+            end
+            
+            op=highestPayoff;
+        end
+        
+        % update
+        overallPayoff(r,c)=overallPayoff(r,c)+op;
+               
+        % strategy mutation
+        if imitationOn
+            % compare overall payoff with von Neumann neighbors
+            highestPayoff=overallPayoff(r,c);
+            highestrc=[r c];
+            for rr=r-1:r+1
+                if rr < 1 || rr > L
+                   continue;
+                end
+                for cc=c-1:c+1
+                    if cc < 1 || cc > L || ...
+                        (rr == r && cc == c) || ... % itself
+                        ((rr==(r-1) || rr==(r+1)) && (cc~=c)) % diagonal
+                       continue;                       
+                    end
+                    
+                    neighbor=grid(rr,cc);
+                    if neighbor==0  % empty
+                        continue;
+                    end
+
+                    if overallPayoff(rr,cc)>highestPayoff
+                        highestPayoff=overallPayoff(rr,cc);
+                        highestrc=[rr cc];
+                    end
+                end
+            end
+
+            % imitate the strategy
+            if highestrc(1) ~= r || highestrc(2) ~= c
+                grid(r,c)=grid(highestrc(1),highestrc(2));
+            end
+        end
+    
+        % noise 1
+        reset = rand();
+        if reset < rProb % random strategy change
+            if rand() < qProb
+                grid(r,c)=cooperator;
+            else
+                grid(r,c)=defector;
+            end
+        end
+    end
+    
+%     str=num2str(t,'Grid, t=%d');
+%     figure('Name',str)
+%     imagesc(grid);
+%     colormap(cmap);
+%     axis square
+end
+
+levelCooperator=sum(sum(grid==cooperator))/size(sitePos,2)
+levelDefector=1-levelCooperator
+
+str=num2str(t,'Grid, t=%d');
+figure('Name',str)
+imagesc(grid);
+colormap(cmap);
+axis square
+
+end
+
+function op = getOverallPayoff(focal, r, c, L, grid, payoff)
+
+op=0;
+
+% interact with von Neumann neighbors
+for rr=r-1:r+1
+    if rr < 1 || rr > L
+       continue;
+    end
+    for cc=c-1:c+1
+        if cc < 1 || cc > L || ...
+            (rr == r && cc == c) || ... % itself
+            ((rr==(r-1) || rr==(r+1)) && (cc~=c)) % diagonal
+           continue;                       
+        end
+        
+        neighbor=grid(rr,cc);
+        if neighbor==0  % empty
+            continue;
+        end
+
+        op=op+payoff(focal, neighbor);
+    end
+end
+
+end
